cff5c6c0b73d79f5f4f7ad1c82e60309c2d97bd9
John Raub <raublekick@gmail.com>
2018-05-18 13:52:19 -0700

    Basic server setup

---
+++ src/bot.js
@@ -0,0 +1,27 @@
+// listen on port so now.sh likes it
+const { createServer } = require('http')
+
+// bot features
+// due to the Twitter ToS automation of likes
+// is no longer allowed, so:
+const Twit = require('twit')
+const config = require('./config')
+const consoleLol = require('console.lol')
+
+const bot = new Twit(config.twitterKeys)
+
+console.rofl('Bot starting...')
+
+// check what day it is and post
+//post()
+//setInterval(post, config.twitterConfig.post)
+
+// This will allow the bot to run on now.sh
+const server = createServer((req, res) => {
+  res.writeHead(302, {
+    Location: `https://twitter.com/${config.twitterConfig.username}`
+  })
+  res.end()
+})
+
+server.listen(3000)
